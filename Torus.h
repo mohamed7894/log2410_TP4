d2c6cde11aa764fc2b392386377c491a1d1d981b
mohamed7894 <medsaddik84@gmail.com>
2018-11-04 12:09:52 -0500

    ms : nouvelle essaie

--- Torus.h
+++ Torus.h
@@ -9,7 +9,7 @@ class Torus : public PrimitiveAbs
 
 public:
 	Torus();
-	virtual ~Torus();
+	virtual ~Torus(const Point3D& pt, f float R, float r, float theta);
 	virtual Torus* clone() const;
 
 	virtual std::size_t getNbParameters() const;
@@ -18,7 +18,7 @@ public:
 
 private:
 	virtual std::ostream& toStream(std::ostream& o) const;
-
+	float m_dimensions[3];
 	
 };
 #endif // !defined(EA_8099F38C_C8A3_47d0_948F_F38A0B83FFB2__INCLUDED_)
